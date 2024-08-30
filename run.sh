e37b8365223749625f21134476ddf2d5ea43173f
tur461 <tur461.er@gmail.com>
2024-08-30 17:16:25 +0530

    feat: add run.sh file
    Signed-off-by: tur461 <tur461.er@gmail.com>

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
+git clone https://github.com/abbas-14/neovim_config
+
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
